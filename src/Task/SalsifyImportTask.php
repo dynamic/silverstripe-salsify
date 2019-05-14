0b7dfac068b5da67c90278f55d7eaf967560d5ec
mak001 <mak001@users.noreply.github.com>
2019-05-14 14:36:06 -0500

    Added fetcher class (#3)
      - currently fetches the url for product export json
      - can use the latest export
    Added Basic task

---
+++ src/Task/SalsifyImportTask.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dynamic\Salsify\Task;
+
+use Dynamic\Salsify\Model\Fetcher;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Dev\BuildTask;
+
+/**
+ * Class ImportTask
+ * @package Dynamic\Salsify\Task
+ */
+class ImportTask extends BuildTask
+{
+    /**
+     * @var string
+     */
+    private static $segment = 'SalsifyImportTask';
+
+    /**
+     * @var string
+     */
+    protected $title = 'Import products from salsify';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Imports products from salsify into silverstripe';
+
+    /**
+     * @var bool
+     */
+    protected $enabled = true;
+
+    /**
+     * @var
+     */
+    private $lineEnding;
+
+    /**
+     * @param \SilverStripe\Control\HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $this->lineEnding = Director::is_cli() ? PHP_EOL : '<br />';
+
+        $channelID = Config::inst()->get(Fetcher::class, 'channel');
+        $fetcher = new Fetcher($channelID, true);
+
+        $fetcher->startExportRun();
+        echo 'Started Salsify export.' . $this->lineEnding;
+
+        $fetcher->waitForExportRunToComplete();
+        echo 'Salsify export complete' . $this->lineEnding;
+
+        echo $fetcher->getExportUrl() . $this->lineEnding;
+        // TODO
+    }
+}
