b9fe3e579a72a42a104c0fd906a2ef6d1750e20b
mak001 <mak001001@gmail.com>
2019-08-09 11:13:54 -0500

    Linted

--- src/Traits/InstanceCreator.php
+++ src/Traits/InstanceCreator.php
@@ -24,7 +24,7 @@ trait InstanceCreator
     /**
      * @return string
      */
-    protected abstract function getImporterKey();
+    abstract protected function getImporterKey();
 
     /**
      * @return string
